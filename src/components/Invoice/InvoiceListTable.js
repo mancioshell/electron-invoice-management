0b2c880f7aa178713d7333ecec1d1f4866150bf0
Alessandro Mancini <dott.alessandro.mancini@gmail.com>
2022-03-11 10:33:39 +0100

    fix: added custom sort function to invoice datetime

--- src/components/Invoice/InvoiceListTable.js
+++ src/components/Invoice/InvoiceListTable.js
@@ -18,50 +18,54 @@ function InvoiceListTable({
   readInvoice,
   printInvoice
 }) {
-  const { t  } = useTranslation(['invoice-list-table'])
+  const { t } = useTranslation(['invoice-list-table'])
 
-  const actions = React.useCallback((invoice) => (
-    <>
-      <Button
-        className="mr-2 mb-2"
-        variant="dark"
-        type="button"
-        size="sm"
-        onClick={(e) => readInvoice(e, invoice)}>
-        <i className="fas fa-clipboard-list"></i> {t('actions.view')}
-      </Button>
-      <Button
-        className="mr-2 mb-2"
-        variant="primary"
-        type="button"
-        size="sm"
-        onClick={(e) => updateInvoice(e, invoice)}>
-        <i className="fas fa-pencil-alt"></i> {t('actions.modify')}
-      </Button>
-      <Button
-        className="mr-2 mb-2"
-        variant="info"
-        type="button"
-        size="sm"
-        onClick={(e) => printInvoice(invoice)}>
-        <i className="fas fa-print"></i> {t('actions.print')}
-      </Button>
-      <Button
-        className="mr-2 mb-2"
-        variant="danger"
-        type="button"
-        size="sm"
-        onClick={(e) => removeInvoice(e, invoice)}>
-        <i className="fas fa-trash"></i> {t('actions.remove')}
-      </Button>
-    </>
-  ), [printInvoice, readInvoice, removeInvoice, updateInvoice, t ])
+  const actions = React.useCallback(
+    (invoice) => (
+      <>
+        <Button
+          className="mr-2 mb-2"
+          variant="dark"
+          type="button"
+          size="sm"
+          onClick={(e) => readInvoice(e, invoice)}>
+          <i className="fas fa-clipboard-list"></i> {t('actions.view')}
+        </Button>
+        <Button
+          className="mr-2 mb-2"
+          variant="primary"
+          type="button"
+          size="sm"
+          onClick={(e) => updateInvoice(e, invoice)}>
+          <i className="fas fa-pencil-alt"></i> {t('actions.modify')}
+        </Button>
+        <Button
+          className="mr-2 mb-2"
+          variant="info"
+          type="button"
+          size="sm"
+          onClick={(e) => printInvoice(invoice)}>
+          <i className="fas fa-print"></i> {t('actions.print')}
+        </Button>
+        <Button
+          className="mr-2 mb-2"
+          variant="danger"
+          type="button"
+          size="sm"
+          onClick={(e) => removeInvoice(e, invoice)}>
+          <i className="fas fa-trash"></i> {t('actions.remove')}
+        </Button>
+      </>
+    ),
+    [printInvoice, readInvoice, removeInvoice, updateInvoice, t]
+  )
 
   const data = React.useMemo(
     () =>
-      invoiceList.map(({ customer, date, _id }) => {
+      invoiceList.map(({ number, customer, date, _id }) => {
         const { name, surname, address } = customer
         return {
+          number,
           name,
           surname,
           address,
@@ -74,10 +78,22 @@ function InvoiceListTable({
 
   const columns = React.useMemo(
     () => [
+      { Header: t('columns.number'), accessor: 'number' },
       { Header: t('columns.name'), accessor: 'name' },
       { Header: t('columns.surname'), accessor: 'surname' },
       { Header: t('columns.address'), accessor: 'address' },
-      { Header: t('columns.date'), accessor: 'date' },
+      {
+        Header: t('columns.date'),
+        accessor: 'date',
+        sortType: (a, b) => {
+          let dateA = a.values.date.split('/')
+          let dateB = b.values.date.split('/')
+          return new Date(`${dateA[1]} ${dateA[0]} ${dateA[2]}`) >
+            new Date(`${dateB[1]} ${dateB[0]} ${dateB[2]}`)
+            ? 1
+            : -1
+        }
+      },
       { Header: t('columns.actions'), accessor: 'actions' }
     ],
     [t]
